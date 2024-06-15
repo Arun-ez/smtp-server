ed9e1f665e07135d51e0c6458fab0086b552921a
Arun <arunshaw433@gmail.com>
2024-06-15 09:21:18 +0530

    added

--- api/index.js
+++ api/index.js
@@ -1,12 +1,29 @@
 import { SMTPServer } from 'smtp-server';
 import { simpleParser } from 'mailparser';
 
+const users = new Set([
+    'info@arunshaw.in'
+])
+
 const server = new SMTPServer({
     authOptional: true,
     allowInsecureAuth: true,
-    // onAuth: (auth, session, callback) => {
-    //     callback(null, { user: 'arunshaw433@gmail.com' })
-    // },
+    onAuth: (auth, session, callback) => {
+
+        if (!auth) {
+            return callback(new Error('Credentials not provided'));
+        }
+
+        if (!auth?.username || !auth.password) {
+            return callback(new Error('username and password are required'));
+        }
+
+        if (!users.has(auth.username)) {
+            return callback(new Error("Account doesn't exist"));
+        }
+
+        return callback(null, { user: auth.username })
+    },
 
     onConnect: (session, callback) => {
         console.log({
@@ -17,24 +34,32 @@ const server = new SMTPServer({
         callback();
     },
 
-    onMailFrom: (address, session, callback) => {
+    onMailFrom: ({ address }, _, callback) => {
         console.log({
             type: 'onMailFrom',
             address,
-            session
         });
 
         callback();
     },
 
-    onRcptTo: (address, session, callback) => {
+    onRcptTo: ({ address }, _, callback) => {
+
+        if (!address) {
+            return callback(new Error('Address not found'));
+        }
+
+        if (!users.has(address)) {
+            return callback(new Error("Account doesn't exist"));
+        }
+
         console.log({
-            type: 'onMailFrom',
+            type: 'onMailTo',
             address,
-            session
+            message: 'Mail sent'
         });
 
-        callback();
+        return callback();
     },
 
     onData: (stream, session, callback) => {
