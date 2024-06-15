e5b234fe3d11b35c1cfe3c2ccd2806b662b3c130
Arun <arunshaw433@gmail.com>
2024-06-15 07:28:40 +0530

    hello world

---
+++ api/index.js
@@ -0,0 +1,48 @@
+import { SMTPServer } from 'smtp-server';
+import { simpleParser } from 'mailparser';
+
+const server = new SMTPServer({
+    authOptional: false,
+    allowInsecureAuth: true,
+    // onAuth: (auth, session, callback) => {
+    //     callback(null, { user: 'arunshaw433@gmail.com' })
+    // },
+
+    onConnect: (session, callback) => {
+        console.log({
+            type: 'onConnect',
+            session
+        });
+
+        callback();
+    },
+
+    onMailFrom: (address, session, callback) => {
+        console.log({
+            type: 'onMailFrom',
+            address,
+            session
+        });
+
+        callback();
+    },
+
+    onRcptTo: (address, session, callback) => {
+        console.log({
+            type: 'onMailFrom',
+            address,
+            session
+        });
+
+        callback();
+    },
+
+    onData: (stream, session, callback) => {
+        simpleParser(stream, (err, data) => {
+            console.log({ data, err, session });
+            callback()
+        })
+    }
+})
+
+server.listen(25, () => { console.log('Server Listening...') })
