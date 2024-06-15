9f0d4a23f57d48103392c67c7eba7b0c4ded91d0
Arun <arunshaw433@gmail.com>
2024-06-15 10:17:08 +0530

    added

---
+++ send.js
@@ -0,0 +1,39 @@
+
+const nodemailer = require('nodemailer');
+
+async function sendEmail() {
+    // Configure the transporter
+    let transporter = nodemailer.createTransport({
+        host: 'mail.arunshaw.in',
+        port: 25,
+        secure: false, // Use SSL/TLS
+        auth: {
+            user: 'info@arunshaw.in', // your email
+            pass: 'password' // your password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // Define email options
+    let mailOptions = {
+        from: '"Example User" <info@arunshaw.in>', // sender address
+        to: 'info@arunshaw.com', // list of receivers
+        subject: 'Hello', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // Send mail
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Message sent: %s', info);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+sendEmail()
